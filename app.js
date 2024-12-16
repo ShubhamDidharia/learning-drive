4e5fba4d3fc468d4334cc7a8b1c5e89927ca84f4
ShubhamDidharia <shubhamdidharia627@example.com>
2024-12-16 21:27:51 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const express=require('express');
 
 const userRoutes=require('./routes/user.routes');
+const indexRoutes=require('./routes/index.routes');
 
 const dotenv=require('dotenv');
 dotenv.config();
 const connectToDb=require('./config/db');
 connectToDb();
 
+const cookieParser=require('cookie-parser');
+
 
 const app=express();
 
 
 app.set("view engine",'ejs')
+app.use(cookieParser());
 app.use(express.json());
+
 app.use(express.urlencoded({extended:true}));
 
 
-app.use('/user', userRoutes)
+app.use('/user', userRoutes);
+app.use('/', indexRoutes);
 
 
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const {body, validationResult}=require('express-validator');
 const userModel=require('../models/user.models');
 const bcrypt =require('bcrypt');
+const jwt=require('jsonwebtoken');
 
 
 
@@ -23,16 +24,48 @@ router.post('/register',
         username:username,
         email:email,
         password:hashPassword
+        
     })
 
     res.json(newUser);
 })
 
 router.get('/login',(req,res)=>{
-    res.render
+    res.render('login');
 })
 
-router.post('/login', async(req,res)=>{
+router.post('/login', 
+    async(req,res)=>{
+    const {username,password}=req.body;
+    const user= await userModel.findOne({
+        username:username
+    })
+    if(!user){
+        return res.status(400).json({
+            
+            message:'invalid username'
+        })
+    }
+
+    // const isMatch= await bcrypt.compare(password,user.password);
+    
+
+    // if(!isMatch){
+    //     res.status(400).json({
+    //         message:'invalid password'
+    //     })
+    // }
+    
+
+
+    const token=jwt.sign({
+        userId:user._id,
+        email:user.email,
+        username:user.username
+    },process.env.JWT_SECRET)
+
+    res.cookie('token',token);
+    res.send('logged in');
 
 })
 
